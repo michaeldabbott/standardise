d56d9a4d6e972da78dc0f9eb3b0a7b99e76e9021
Michael Abbott <michael@mabbott.dev>
2022-01-06 19:37:12 +0000

    version 1:

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/michaeldabbott/standardise
 
 go 1.16
 
-require github.com/go-chi/chi/v5 v5.0.7 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.0.7
+	github.com/opentracing/opentracing-go v1.2.0
+	go.uber.org/zap v1.20.0
+)
--- pkg/server/option.go
+++ pkg/server/option.go
@@ -3,3 +3,17 @@ package server
 type Option interface{ apply(s *Server) }
 
 type FactoryOption interface{ apply(f *factory) }
+
+
+// WithLogger provides option to provide a logger implementation. Noop is default
+func WithLogger(l Logger) FactoryOption { return loggerOption{logger: l} }
+
+// WithTracer provides option to provide a tracer implementation. Noop is default
+func WithTracer(t opentracing.Tracer) FactoryOption { return tracerOption{tracer: t} }
+
+// WithConfig provides option to provide a server configuration.
+func WithConfig(c Config) FactoryOption { return configOption{c} }
+
+// WithRouter provides option to provide a function which returns which router will be used.
+// By default we use http.ServeMux
+func WithRouter(rf func() Handler) FactoryOption { return routerOption{rf} }
